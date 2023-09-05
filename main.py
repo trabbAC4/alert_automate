8abc130ef988d3a788bf4184a0c430b236c329d7
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-05 19:44:41 -0400

    configuration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import datetime
 import os.path
 
+from dateutil import parser
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -35,12 +36,15 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+def commitHours(creds):
     try:
         service = build('calendar', 'v3', credentials=creds)
 
         # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
+        now = datetime.datetime.today()# 'Z' indicates UTC time
+        timeStart = str(now) + "T10:00:00Z"
+        timeEnd = str(now) + "T23:59:59Z"
+        print("Getting today's coding hours")
         events_result = service.events().list(calendarId='primary', timeMin=now,
                                               maxResults=10, singleEvents=True,
                                               orderBy='startTime').execute()
@@ -49,11 +53,26 @@ def main():
         if not events:
             print('No upcoming events found.')
             return
-
-        # Prints the start and name of the next 10 events
+        
+        total_duration = datetime.timedelta(
+            seconds= 0,
+            minutes = 0,
+            hours= 0, 
+        )
+        id = 0
+        print("Coding Hours:")
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+            end = event['end'].get('dateTime', event['end'].get('date'))
+
+            start_formatted = parser.isoparse(start)
+            end_formatted = parser.isoparse(end)
+            duration = end_formatted - start_formatted
+
+            total_duration += duration
+            print(f"{event['summary']}, duration: {duration}")
+        
+        print(f"Total time: {total_duration}")
 
     except HttpError as error:
         print('An error occurred: %s' % error)
